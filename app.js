4ca7ff8b714031d797bee18da447e69d16d12ee9
emiresen <emiresen246@gmail.com>
2024-04-17 20:08:14 +0300

    form added

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-let il = prompt("Il giriniz: ");
-let ilce = prompt("Ilce giriniz: ");
+// let il = prompt("Il giriniz: ");
+// let ilce = prompt("Ilce giriniz: ");
+
+const form = document.querySelector("form");
+let il = document.querySelector("#city");
+let ilce = document.querySelector("#area");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+})
+
 
 let xhr = new XMLHttpRequest();
 xhr.withCredentials = false;
@@ -7,41 +17,42 @@ xhr.withCredentials = false;
 const userlistDiv = document.getElementById('user_list');
 
 xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-        const response = JSON.parse(this.responseText);
-        const pharmacies = response.result;
+  if (this.readyState === this.DONE) {
+    const response = JSON.parse(this.responseText);
+    const pharmacies = response.result;
 
-        pharmacies.forEach(eczane => {
-            userlistDiv.innerHTML += `<div class="col-3">
-                    <div class="card">
-                        <div style="width: 100%">
-                            <iframe
-                                width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-                            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=${eczane.loc}+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">
-            ${eczane.name}
-          </h5>
-          <p class="card-text">
-            ${eczane.address}
-          </p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">
-            ${eczane.phone}
-          </li>
-          <li class="list-group-item">${eczane.loc}</li>
-          <li class="list-group-item">${eczane.dist}</li>
-        </ul>
+    pharmacies.forEach(eczane => {
+      userlistDiv.innerHTML += `<div class="col-3">
+                  <div class="card">
+                      <div style="width: 100%">
+                          <iframe
+                              width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+                          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=${eczane.loc}+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
       </div>
-    </div>`;
-        });
-    }
+      <div class="card-body">
+        <h5 class="card-title">
+          ${eczane.name}
+        </h5>
+        <p class="card-text">
+          ${eczane.address}
+        </p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+          ${eczane.phone}
+        </li>
+        <li class="list-group-item">${eczane.loc}</li>
+        <li class="list-group-item">${eczane.dist}</li>
+      </ul>
+    </div>
+  </div>`;
+    });
+  }
 });
 
-xhr.open("GET", `https://api.collectapi.com/health/dutyPharmacy?ilce=${ilce}&il=${il}`);
+xhr.open("GET", `https://api.collectapi.com/health/dutyPharmacy?ilce=${ilce.value}&il=${il.value}`);
 xhr.setRequestHeader("content-type", "application/json");
-xhr.setRequestHeader("authorization", "");
+xhr.setRequestHeader("authorization", "apikey 61TPvxVJelurTdRIcKt2G0:78zGb0eR1Cc0QmHJ89eytf");
 
 xhr.send();
+
